56c578efa44c7dd44fd9efb14ffe71a2348519dc
Justin Lui <justinlui17@gmail.com>
2023-11-28 14:50:50 -0800

    Print board

--- sudoku.cpp
+++ sudoku.cpp
@@ -15,10 +15,35 @@ int getIntInput(std::string message) {
     return x;
 }
 
-int main() {
-    int row = getIntInput("Row: ");
-    int col = getIntInput("Col: ");
+void printBoard(std::vector<std::vector<int> > board) {
+    for (int r = 1; r < 10; r++) {
+        for (int c = 1; c < 10; c++) {
+            std::cout << board[r - 1][c - 1];
+            if (c != 9)
+                if (c % 3 == 0)
+                    std::cout << "||";
+                else
+                    std::cout << "|";
+            else
+                std::cout << "\n";
+        }
 
-    std::cout << "Row: " << row << " Col: " << col;
+        if (r != 9) {
+            if (r % 3 == 0)
+                std::cout << "= = =  = = =  = = =\n";
+            else
+                std::cout << "- - -  - - -  - - -\n";
+        }
+    }
+}
+
+int main() {
+    std::vector<std::vector<int> > board(9, std::vector<int>(9, 0));
+    printBoard(board);
+
+    // int row = getIntInput("Row: ");
+    // int col = getIntInput("Col: ");
+
+    // std::cout << "Row: " << row << " Col: " << col;
     return 0;
 }
